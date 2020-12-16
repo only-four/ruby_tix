eb18d88cb26e9a8bc273120a8147860db8410228
Annie <annielin28815@gmail.com>
2020-12-16 16:11:20 +0800

    set user select

--- app/javascript/navbar/user.js
+++ app/javascript/navbar/user.js
@@ -1,10 +1,11 @@
 console.log('hi')
 
 document.addEventListener('DOMContentLoaded', function(){
-  select = document.querySelector('.log_in')
+  select = document.querySelector('.current_user_account_name')
   select.addEventListener('change',function(){
     if(select.value == "2"){
-      fetch('')
+      console.log('aaaaaaaaaa');
+      window.location.href = '/users/edit';
     }
   })
 })
--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module RubyTix
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.hosts << 'f293e770e464.ngrok.io'
+    config.hosts << 'd255c36622b2.ngrok.io'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
