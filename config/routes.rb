7537670de6abcc19cfc3cd6d4fae71fdd74e70ad
Jane <m3281@hotmail.com>
2020-12-16 14:03:53 +0800

    add admin/tickets & tickets routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'admin/tickets#index'
+  resources :tickets, only: [:index, :show]
+  namespace :admin do
+    resources :tickets, except: [:show]
+  end
 end
