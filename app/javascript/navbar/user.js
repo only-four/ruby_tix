c99abb3844b64b81d7adb57539f8629ce87068dd
Annie <annielin28815@gmail.com>
2020-12-20 02:51:26 +0800

    add user's and activity's category

--- app/javascript/navbar/user.js
+++ app/javascript/navbar/user.js
@@ -3,20 +3,52 @@ document.addEventListener('DOMContentLoaded', function(){
   select = document.querySelector('.current_user_account_name')
 
   select.addEventListener('change',function(){
-    if(select.value == "2"){
-      console.log('aaaaaaaaaa');
-      window.location.href = '/users/edit';
-    }else if(select.value == "5"){
-      console.log('hihihihhi');
+    if(select.value === "2"){
+      console.log('2');
+      $(`.centre`).show();
+      $(`.hold`).hide();
+      $(`.record`).hide();
+      $(`#member_centre`).on('click', function(){
+        $(`.centre`).show();
+      })
+    }else if(select.value === "3"){
+      console.log('3');
+      $(`.record`).show();
+      $(`.centre`).hide();
+      $(`.hold`).hide();
+      $(`#activity_record`).on('click', function(){
+        $(`.record`).show();
+      })
+    }else if(select.value === "4"){
+      console.log('4');
       $(`.hold`).show();
-      $(`#hold_activity`).click(function(){
+      $(`.centre`).hide();
+      $(`.record`).hide();
+      $(`#hold_activity`).on('click', function(){
         $(`.hold`).show();
       })
+    }else if(select.value === "5"){
+      console.log('4');
+      $(`.cart`).show();
+      $(`.centre`).hide();
+      $(`.record`).hide();
+      $(`.hold`).hide();
+      $(`#shopping_cart`).on('click', function(){
+        $(`.cart`).show();
+      })
     }
+
+
+
   })
 })
 
 
+
+    // if(select.value == "2"){
+    //   console.log('2');
+    //   window.location.href = '/users/edit';
+    // }
 
 // var hold = document.getElementById("hold_activity"); 
 // var toggle_hold = function() {  
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,14 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-import 'jquery'
-
-$().ready(function(){
-  console.log("You're so sweet!")
-})
-
-
 require("navbar")
+import 'jquery'
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
