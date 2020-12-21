7a94b3159c375bf8b88f4b61b83aa1eeb53d82e3
Jane <m3281@hotmail.com>
2020-12-21 21:26:22 +0800

    modify ticket htmls

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,5 @@
 class TicketsController < ApplicationController
+  def index
+    @ticket_types= TicketType.where(ticket_id: 18)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'admin/tickets#index'
+  
+  # 票券顯示
   resources :tickets, only: [:index, :show]
+  
+  # 後台票券crud
   namespace :admin do
     resources :tickets, except: [:show]
   end
