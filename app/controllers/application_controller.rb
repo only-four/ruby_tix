01b4f812c74c10c048c234294d1e6cbb0fab1e2c
Jane <m3281@hotmail.com>
2020-12-24 13:20:50 +0800

    modify appcon

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound, with: :no_record_found
+
+
+  private
+  def no_record_found
+    render plain: "sorry, the record you're looking for doesn't exist.", status: 404
+  end
 end
