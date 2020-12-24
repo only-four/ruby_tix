362343010f0ce905595e50e484cd08d06579d05b
Jane <m3281@hotmail.com>
2020-12-24 17:09:50 +0800

    add bootstrap fony awsome

--- app/controllers/admin/activities_controller.rb
+++
@@ -1,9 +0,0 @@
-class Admin::ActivitiesController < ApplicationController
-  def index
-    @activities = Activity.all
-  end
-  def new
-    @activity = Activity.new
-    2.times { @activity.ticket_types.build }
-  end
-end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,9 @@ require("@rails/activestorage").start()
 require("channels")
 require("navbar")
 import 'jquery'
-
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
+import "@fortawesome/fontawesome-free/js/all"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
