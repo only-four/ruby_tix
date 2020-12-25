c4f2c3a899ea515613e09cca5a821aaf213bc311
Jane <m3281@hotmail.com>
2020-12-25 15:26:13 +0800

    add tailwind

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,6 +17,12 @@ import $ from 'jquery';
 window.$ = $
 import "controllers"
 
+// tailwind
+import "stylesheets/application" // ADD THIS LINE
+
+
+
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
