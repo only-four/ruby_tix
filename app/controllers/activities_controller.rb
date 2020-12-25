c7c91f10c81f61cb27215165a8fb580d9e93b992
kihifung <kihifung@gmail.com>
2020-12-25 10:16:57 +0800

    update to latest

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -18,12 +18,14 @@ class ActivitiesController < ApplicationController
   def edit
     @activity = Activity.find_by(id: params[:id])    
   end
-
+  def destroy
+    @activity = Activity.destroy  
+  end
   private
   def activity_params
     params.require(:activity).permit(:user, :title, :begin_datetime, :finish_datetime,
        :location, :content, :brief, :tag, :link, :form, :notice,  :phone, :email, :other_contect, 
-       :limit, :free_ticket, :vip_ticket, :pr_ticket  )
+       :limit, :free_ticket, :vip_ticket, :pr_ticket, address_attributes: [:location, :id, :_destroy]  )
   end
 end
 
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,4 +3,5 @@ class Activity < ApplicationRecord
   has_many :activity_users
   has_many :tickets
   has_many :comments
+  accepts_nested_attributes_for :tickets, allow_destroy: true, reject_if: :all_blank
 end
