9cca44e442f4c44c59d0163fea0afa85645046c6
Jane <m3281@hotmail.com>
2021-01-01 12:25:26 +0800

    modify code

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,22 +1,25 @@
 class CommentsController < ApplicationController
+  before_action :find_activity
   before_action :authenticate_user!
   # acts_as_paranoid without_default_scope: true  
   
   def create
     p params
-    @activity = Activity.find(params[:activity_id])  
     @comment = @activity.comments.new(comment_params)  
     @comment.user = current_user
     @comment.save
   end
 
   def destroy
-    @activity = Activity.find(params[:activity_id])      
     @comment = @activity.comments.find(params[:id])
     @comment.destroy
   end
 
   private
+  def find_activity
+    @activity = Activity.find(params[:activity_id])      
+  end
+
   def comment_params
     params.require(:comment).permit( :content, :activity_id)
   end
