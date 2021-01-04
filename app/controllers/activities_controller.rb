0d2db56d9dd458d04411b68a6709ac594600ca6b
kihifung <kihifung@gmail.com>
2021-01-04 18:21:08 +0800

    fix time/deploy problem

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,5 @@
 class ActivitiesController < ApplicationController
-    # before_action :find_activity, :only []
+    before_action :find_activity, only: [:join, :edit, :destroy, :update, :show]
 
     def index
       @activities = Activity.all
@@ -13,23 +13,22 @@ class ActivitiesController < ApplicationController
       @activity = Activity.new(activity_params)
       if @activity.save
         redirect_to activities_path(@activity.id)  , notice: "新增活動成功！ 請繼續新增活動票種"
-        # redirect_to activities_path, notice: "新增活動成功！", "請繼續新增活動票種"
        else
         render :new
       end
     end
 
     def join
-      @activity = Activity.find(params[:id])
+      # @activity = Activity.find_by(id: params[:id])
       @activity.activity_users.create if @activity
       redirect_to activities_path, notice: "報名完成！"
     end
 
     def edit 
-      @activity = Activity.find(params[:id])
+      # @activity = Activity.find_by(id: params[:id])
     end  
     def update
-      @activity = Activity.find(params[:id])
+      # @activity = Activity.find_by(id: params[:id])
       if @activity.update(activity_params)
         redirect_to activities_path(@activity), notice: "資料更新成功!"      
       else
@@ -38,19 +37,14 @@ class ActivitiesController < ApplicationController
     end
   
     def show
-      @activity = Activity.find(params[:id])
+      # @activity = Activity.find_by(id: params[:id])
       @comment= @activity.comments.new
       # show comments
-      @activity = Activity.find(params[:id])
       @comments= activity.comments.order(updated_at: :desc)
     end
 
-    def date_field
-      change(min: 0)
-    end  
-
     def destroy
-      @activity = Activity.find(params[:id])
+      # @activity = Activity.find_by(id: params[:id])
       @activity.destroy if @activity
       redirect_to activities_path, notice: "活動資料已刪除!"
     end
@@ -77,8 +71,8 @@ class ActivitiesController < ApplicationController
       ticket_types_attributes: [:id, :title, :content, :quantity, :sell_start, :sell_deadline, :price, :_destroy],
       address_attributes: [:location, :id, :_destroy]  )
   end 
-  # def find_activity
-  #   @activity = Activity.find_by(id: params[:id])
-  # end
+  def find_activity
+    @activity = Activity.find_by(id: params[:id])
+  end
 
 end
