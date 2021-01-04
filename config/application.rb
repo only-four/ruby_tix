0ffa572c8937d2c43adfc33329e17b6d6bee8a4f
kihifung <kihifung@gmail.com>
2021-01-04 18:39:44 +0800

    add config host

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module RubyTix
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.hosts << "www.rubytix.fun"
+    config.hosts << "warm-earth-15284.herokuapp.com"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
