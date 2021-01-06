0c3add4726be2fcdae7439a0ba803750601d350a
Chun-Hsiang Yu <kihifung@gmail.com>
2021-01-06 16:12:10 +0800

    Update application.js
    delete annotation

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -21,7 +21,6 @@ import 'bootstrap/dist/css/bootstrap.css'
 import "@fortawesome/fontawesome-free/js/all"
 import 'jquery'
 import $ from 'jquery';
-// import "qr_code_reader.js"
 window.$ = $
 import './qr_code_reader'
 
