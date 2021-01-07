1383e78313023516ac5ebd8570f1c8600eb69552
kihifung <kihifung@gmail.com>
2021-01-08 03:13:59 +0800

    fix time zone

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,8 @@ module RubyTix
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.hosts << "www.rubytix.fun"
-    config.hosts << "warm-earth-15284.herokuapp.com"
-    config.hosts << "rocky-caverns-12313.herokuapp.com"
-    
+    config.hosts << "peaceful-forest-86679.herokuapp.com"
+    config.time_zone = "Taipei"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
