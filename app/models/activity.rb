08dcc38130ba5c0b8b9fe4f98ddc48eddecb9cd4
kihifung <kihifung@gmail.com>
2021-01-08 03:09:31 +0800

    feature activity aasm

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -10,4 +10,32 @@ class Activity < ApplicationRecord
   # ticket_type 寫在activity頁面 巢狀表單
   accepts_nested_attributes_for :ticket_types, allow_destroy: true, reject_if: :all_blank
   mount_uploader :image, ImageUploader
-end
+
+  enum is_available: {
+    prepare: 1,
+    ongoing: 2,
+    expired: 3
+   }
+
+   aasm column: :state do
+    state :prepare, initial: true
+    state :ongoing, :expired, :canceled, :deleted
+
+    event :ongo do
+      transitions from: :prepare, to: :ongoing
+    end
+
+    event :expire do
+      transitions from: :ongoing, to: :expired
+    end
+
+    event :cancel do
+      transitions from: :ongoing, to: :canceled
+    end
+
+    event :delete do
+      transitions from: [:prepare, :expired], to: :deleted
+    end
+
+  end   
+
