a10ef926daf2002f09b9c10290aaa6d6236e7f9f
Jane <m3281@hotmail.com>
2021-01-10 15:10:59 +0800

    adj link

--- app/controllers/qr_codes_controller.rb
+++ app/controllers/qr_codes_controller.rb
@@ -2,29 +2,32 @@ class QrCodesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_qr_data, only: :create
 
-  def attand_list
-    @qr_codes = QrCode.order(created_at: :desc).first(20)
-  end
-
   def scan
   end
 
   def create         
-    qr_code = QrCode.create(data: @qr_data)
-    redirect_to participated_qr_code_path(qr_code)
-    ticket.use!       
-    # ticket= Ticket.find_by(ticket_number: @qr_data)
-    # if ticket.state == "activated"
-    # else
-    #   render :scan
-    # end
+    ticket= Ticket.find_by(ticket_number: @qr_data)
+    if ticket.state == "activated"
+      qr_code = QrCode.create(data: @qr_data)
+      redirect_to participated_qr_code_path(qr_code)
+      ticket.use!       
+    else
+      render :scan
+    end
   end
 
   def participated
     @qr_code = QrCode.find(params[:id])
     @current_ticket= Ticket.find_by(ticket_number:@qr_code.data)
-    @current_tickettype = TicketType.find(id:@current_ticket.ticket_type_id)
-    @current_activity = Activity.find(id:@current_tickettype.activity_id)
+    @current_tickettype = TicketType.find_by(id:@current_ticket.ticket_type_id)
+    @current_activity = Activity.find_by(id:@current_tickettype.activity_id)
+  end
+
+  def attand_list
+    @qr_codes = QrCode.order(created_at: :desc).first(20)
+    # @current_ticket= Ticket.find_by(ticket_number:@qr_code.data)
+    # @current_tickettype = TicketType.find_by(id:@current_ticket.ticket_type_id)
+    # @current_activity = Activity.find_by(id:@current_tickettype.activity_id)
   end
 
   private
