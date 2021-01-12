8f5067e952786b0b89f66bf296bbe6f53f2d281c
samliu <samliuforwork@gmail.com>
2021-01-12 20:40:58 +0800

    fix channel conflict

--- app/frontend/javascript/index.js
+++ app/frontend/javascript/index.js
@@ -1,17 +1,8 @@
 import "./user"
 import "./chatbox"
 import "./comment"
-<<<<<<< HEAD
-import "./bundle"
-
-
-
-// import "./styles/navbar/users.scss"
-// import "./styles/mainbar/activitiy_category.scss"
-=======
 import "./ticket_types"
 import "./search"
 import "./qr_code"
 import "./qr_code_reader"
 import "qrcode"
->>>>>>> dev
