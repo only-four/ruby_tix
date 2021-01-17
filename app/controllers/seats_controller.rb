858c8609feffac7695e1aeaa4c0a4edcdb9d74de
Jane <m3281@hotmail.com>
2021-01-17 15:46:44 +0800

    seat js

--- app/controllers/seats_controller.rb
+++ app/controllers/seats_controller.rb
@@ -1,8 +1,17 @@
 class SeatsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_seat_params ,only: :update
+
   def show
     @seats = Seat.all.order(id: :asc)
   end
-  # def update
-  #   Seat.find_by(seat_num: "#").update(available:"F", taken_by: current_user.id)
-  # end
+
+  def update
+    Seat.find_by(seat_num: seat_params[:seat_number]).update(available:"F", taken_by: current_user.id)
+  end
+
+  private
+  def set_seat_params
+    seat_params= JSON.parse(params[:data])
+  end
 end
