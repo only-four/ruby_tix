194b1baf38b65d4aa2fcabf1d0fa5beaa11755c3
kihifung <kihifung@gmail.com>
2021-01-18 15:25:43 +0800

    add slide_loop
    - add slide_loop
    - fix typo
    - delect temp files

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -27,4 +27,16 @@ import Swiper, { Navigation, Pagination } from 'swiper';
 
 // configure Swiper to use modules
 Swiper.use([Navigation, Pagination]);
-
+var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+});
