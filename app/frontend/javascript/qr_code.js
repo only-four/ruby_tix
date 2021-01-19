25824585001d09593d0efa8860b190c7bab5aee8
Jane <m3281@hotmail.com>
2021-01-19 15:24:30 +0800

    qr code size

--- app/frontend/javascript/qr_code.js
+++ app/frontend/javascript/qr_code.js
@@ -3,7 +3,7 @@ document.addEventListener("turbolinks:load", function(){
   var canvases = document.querySelectorAll('.qr-canvas')
   canvases.forEach(function(canvas) {
     var tkt_num = canvas.dataset.id
-    QRCode.toCanvas(canvas, `${tkt_num}`, function(error){
+    QRCode.toCanvas(canvas, `${tkt_num}`,{ width:190, height:190 }, function(error){
       if (error) {
         console.error(error)
       } 
