89848346bd912b400a25f3604b95614befa9ead2
Jane <m3281@hotmail.com>
2021-01-21 00:55:55 +0800

    add activity title to cart.html

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -11,7 +11,7 @@ class Activity < ApplicationRecord
   belongs_to :creator, foreign_key: :user_id, class_name: 'User'
   # belongs_to :category
   
-  validates :hostname, :title, :content, :begin_datetime, :finish_datetime, :phone, :email, :limit, presence: true
+  # validates :hostname, :title, :content, :begin_datetime, :finish_datetime, :phone, :email, :limit, presence: true
 
   # ticket_type 寫在activity頁面 巢狀表單
   accepts_nested_attributes_for :ticket_types, allow_destroy: true, reject_if: :all_blank
