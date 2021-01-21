893fce382118319044335f7b23230ba901e73f34
Jane <m3281@hotmail.com>
2021-01-21 17:32:07 +0800

    add migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_21_072531) do
+ActiveRecord::Schema.define(version: 2021_01_21_092904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -179,6 +179,7 @@ ActiveRecord::Schema.define(version: 2021_01_21_072531) do
     t.string "data"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "event"
   end
 
   create_table "roles", force: :cascade do |t|
