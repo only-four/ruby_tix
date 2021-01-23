943e90e8b9112a89cfb6f07a056ab44d43979b8a
Jane <m3281@hotmail.com>
2021-01-23 14:20:44 +0800

    heroku host

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,8 +5,7 @@ CarrierWave.configure do |config|
     aws_access_key_id:     ENV["aws_access_key_id"], 
     aws_secret_access_key: ENV["aws_secret_access_key"], 
     region: 'ap-northeast-1', #optional, defaults to 'us-east-1'
-    path_style: 'true',
-    # host: 's3-ap-northeast-1.amazonaws.com',
+    host: 's3-ap-northeast-1.amazonaws.com',
     # use_iam_profile: true #optional, defaults to nil
   }
   config.fog_directory  = ENV["fog_directory"]           # required
