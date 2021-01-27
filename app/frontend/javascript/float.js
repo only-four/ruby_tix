26b993bbea51bb36d8d31fdbcbffa190d86c84ba
Annie <annielin28815@gmail.com>
2021-01-27 20:23:05 +0800

    add activity join area

--- app/frontend/javascript/float.js
+++ app/frontend/javascript/float.js
@@ -1,6 +1,8 @@
 document.addEventListener("turbolinks:load", function(){
   //Get the button:
   topbutton = document.getElementById("topBtn");
+  // addActivityArea = document.getElementById("js-add-ac-area");
+
 
   // When the user scrolls down 20px from the top of the document, show the button
   window.onscroll = function() {scrollFunction()};
@@ -8,15 +10,22 @@ document.addEventListener("turbolinks:load", function(){
   function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
       topbutton.style.display = "block";
+      addActivityArea.style.display = "block";
     } else {
       topbutton.style.display = "none";
+      addActivityArea.style.display = "none";
     }
   }
 
-  // When the user clicks on the button, scroll to the top of the document
   topbutton.addEventListener('click', function() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   })
+
+
+
+  
+
+
 
 })
