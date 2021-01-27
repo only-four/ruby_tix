793639bda7c585e30dcad3e8336d16ec067c86c2
kihifung <kihifung@gmail.com>
2021-01-27 20:16:46 +0800

    fix tailwind problem

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -1,28 +1,26 @@
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("javascript")
-require("stylesheets")
-require("trix")
-require("@rails/actiontext")
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("javascript");
+require("stylesheets");
+require("trix");
+require("@rails/actiontext");
 // import "stimulus"
-import "controllers"
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'jquery'
-import $ from 'jquery';
-window.$ = $
-import "chart.js"
-import Swal from 'sweetalert2';
-window.Swal= Swal
+import "controllers";
+import "bootstrap";
+import "jquery";
+import $ from "jquery";
+window.$ = $;
+import "chart.js";
+import Swal from "sweetalert2";
+window.Swal = Swal;
 // window.Swiper = require('swiper');
-window.Swiper = require('swiper/swiper-bundle');
+window.Swiper = require("swiper/swiper-bundle");
 
 // import Swiper styles
-import 'swiper/swiper-bundle.css';
+import "swiper/swiper-bundle.css";
 // core version + navigation, pagination modules:
-import Swiper, { Navigation, Pagination } from 'swiper';
-
+import Swiper, { Navigation, Pagination } from "swiper";
 
 // configure Swiper to use modules
 Swiper.use([Navigation, Pagination]);
