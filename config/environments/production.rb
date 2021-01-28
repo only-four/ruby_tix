2b9ad95b52282204374abfdfda53d3daa3139bb7
samliu <samliuforwork@gmail.com>
2021-01-28 15:50:37 +0800

    deploy_add prodution

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.hosts << "damp-crag-35739.herokuapp.com"
   # Code is not reloaded between requests.
   config.cache_classes = true
 
