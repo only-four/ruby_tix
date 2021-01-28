17ac29c5aa3d122bf0e03a5389d6d139bb5acad1
Jane <m3281@hotmail.com>
2021-01-28 16:57:47 +0800

    f

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -22,6 +22,5 @@ import 'swiper/swiper-bundle.css';
 // core version + navigation, pagination modules:
 import Swiper, { Navigation, Pagination } from 'swiper';
 
-
 // configure Swiper to use modules
 Swiper.use([Navigation, Pagination]);
