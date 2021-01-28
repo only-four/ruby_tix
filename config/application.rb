33bb4d7b8420a6d7b76c722436d54180c5c2f11c
kihifung <kihifung@gmail.com>
2021-01-28 17:04:46 +0800

    add config host

--- config/application.rb
+++ config/application.rb
@@ -10,9 +10,9 @@ module RubyTix
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.hosts << "rubytix.herokuapp.com"
+    config.hosts << "rubytix.space"
     config.hosts << "www.rubytix.space"
     config.hosts << "www.rubytix.fun"
-    config.hosts << "peaceful-forest-86679.herokuapp.com"
     config.hosts << "damp-cove-37711.herokuapp.com"
     config.hosts << "infinite-scrubland-08013.herokuapp.com"
     config.hosts << "b8fcb5b3c787.ngrok.io"
